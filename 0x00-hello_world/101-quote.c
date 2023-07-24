ef56097e830c0f1a7e6492e26f6c35293f7be09a
toromoh23 <akanbijuliet5@gmail.com>
2023-07-24 09:26:20 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,11 @@
 #include <unstd.h>
 /**
  * main - Entry point
- * Return: Always 1 (Success)
+ * Return: 1 (Success)
  */
 
 int main(void)
 {
-	write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
